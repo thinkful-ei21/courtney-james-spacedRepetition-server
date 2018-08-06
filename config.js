557ac3c735c32ea6b649c9f486b76d243d119fdf
James Stuckey <jstuckey@rutgers.edu>
2018-08-06 14:14:39 -0400

    db connected

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://webuser1:repitition123@ds115022.mlab.com:15022/courtney-james-spaced-repitition',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-// const { dbConnect } = require('./db-mongoose');
+const { dbConnect } = require('./db-mongoose');
 const routeApp = require('./routes/index');
 
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+        skip: (req, res) => process.env.NODE_ENV === 'test'
+    })
 );
 
 app.use(bodyParser.json());
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+    cors({
+        origin: CLIENT_ORIGIN
+    })
 );
 
 app.use(routeApp);
 
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+    const server = app
+        .listen(port, () => {
+            console.info(`App listening on port ${server.address().port}`);
+        })
+        .on('error', err => {
+            console.error('Express failed to start');
+            console.error(err);
+        });
 }
 
 if (require.main === module) {
-  // dbConnect();
-  runServer();
+    dbConnect();
+    runServer();
 }
 
 module.exports = { app };
