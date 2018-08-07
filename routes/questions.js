7ed452f62b2e07ea2667e7d0d1ebf18815796923
James Stuckey <jstuckey@rutgers.edu>
2018-08-07 14:43:59 -0400

    added Jwt auth to questions route

--- routes/questions.js
+++ routes/questions.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
 
 const User = require('../models/users');
 const Question = require('../models/question');
+
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 router.get('/', (req, res, next) => {
     Question.find().then(results => {
