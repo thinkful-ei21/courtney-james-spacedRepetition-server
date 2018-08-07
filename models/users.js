46a84ec96a84cf17c527d1c60436e46505a1cb10
James Stuckey <jstuckey@rutgers.edu>
2018-08-07 11:10:37 -0400

    updated user password to be required

--- models/users.js
+++ models/users.js
@@ -1,27 +1,28 @@
+'use strict';
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  password: String
-})
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
 
 userSchema.set('toObject', {
-  virtuals: true,
-  versionKey: false,
-  transform: (doc, ret) => {
-    delete ret._id;
-    delete ret.password;
-  }
+    virtuals: true,
+    versionKey: false,
+    transform: (doc, ret) => {
+        delete ret._id;
+        delete ret.password;
+    }
 });
 
 userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compare(password, this.password);
-}
+    return bcrypt.compare(password, this.password);
+};
 
 userSchema.statics.hashPassword = function(password) {
-  return bcrypt.hash(password, 10);
-}
-
+    return bcrypt.hash(password, 10);
+};
 
 module.exports = mongoose.model('User', userSchema);
